aae64aea1a111993bc99cce5e45bb9cd67a3d52c
Samuel Terra <samuelterra22@hotmail.com>
2020-10-11 11:19:24 -0300

    Add babel and styled-components

---
+++ src/pages/_app.tsx
@@ -0,0 +1,8 @@
+import GlobalStyle from "../styles/GlobalStyle";
+
+export default function App({Component, pageProps}) {
+    return (<>
+        <GlobalStyle />
+        <Component {...pageProps} />
+    </>)
+}
