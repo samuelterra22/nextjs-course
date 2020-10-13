92ae0b2f46938cd0efb5adddb6a6da2dc106a66d
Samuel Terra <samuelterra22@hotmail.com>
2020-10-12 21:46:21 -0300

    Add example of dynamic import

--- src/pages/catalog/products/[slug].tsx
+++ src/pages/catalog/products/[slug].tsx
@@ -1,7 +1,25 @@
 import {useRouter} from "next/router";
+import React, {useState} from "react";
+import dynamic from "next/dynamic";
+
+const AddToCartModal = dynamic(
+    () => import('../../../components/AddToCartModal'),
+    {loading: () => <p>Carregando...</p>}
+);
 
 export default function Product() {
     const router = useRouter();
+    const [isAddToCartModalVisible, setIsAddToCartModalVisible] = useState(false)
 
-    return <h1>{router.query.slug}</h1>
+    function handleAddToCard() {
+        setIsAddToCartModalVisible(true)
+    }
+
+    return (<div>
+        <h1>{router.query.slug}</h1>
+
+        <button onClick={handleAddToCard}>Add to card</button>
+
+        {isAddToCartModalVisible && <AddToCartModal/>}
+    </div>)
 }
